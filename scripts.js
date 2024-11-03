19a74ff0076814e72ac2f12a80fbd300604d1585
antonioisaacdias <antonioisaacvd@gmail.com>
2024-11-03 19:44:46 -0300

    Ajustes finais no script e adição de feature: agora ao fechar a janela os items remanescentes são salvos no localStorage e recuperados em um futuro acesso.

--- scripts.js
+++ scripts.js
@@ -6,40 +6,67 @@ const warningBtnClose = document.querySelector(".warning-wrapper button");
 const alertBox = document.querySelector(".alert");
 const alertBoxBtnClose = document.querySelector(".alert-box button")
 
+document.addEventListener("DOMContentLoaded", function(){
+    LIST_LOCAL.refreshItems();
+    LIST_LOCAL.showItems();
+
+    LIST_LOCAL.items.forEach(function(item){
+        createItemList(item);
+    })
+});
+
+const LIST_LOCAL = {
+    _name: "listLocal",
+    _items: [],
+
+    getName () {
+        return this.name;
+    },
+
+    get Items() {
+        return this._items;
+    },
+
+    showItems () {
+        console.log("Items da sessão atual:")
+        console.log(this.items);
+        console.log("Items no local Storage:")
+        console.log(JSON.parse(localStorage.getItem("listLocal")).items)
+    },
+
+    addItem (item) {
+        this.items.push(item);
+        localStorage.setItem("listLocal", JSON.stringify(this));
+    },
+
+    removeItem (item) {
+        this.items.splice(this.items.indexOf(item), 1);
+        localStorage.setItem("listLocal", JSON.stringify(this));
+    },
+
+    refreshItems () {
+        const localStorageItems = JSON.parse(localStorage.getItem("listLocal")).items;
+        this.items = localStorageItems;
+    },
+
+    clearLocalStorage () {
+        this.items = [];
+        localStorage.setItem("listLocal", JSON.stringify(this));
+    }
+    
+};
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
     if (newItem.value == "") {
         alertBox.style.display = "flex";
     } else {
-            
-    const li = document.createElement("li");
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    const checkImage = document.createElement("span");
-    checkImage.classList.add("check-image");
-
-    const checkWrapper = document.createElement("span");
-    checkWrapper.classList.add("check-wrapper");
-    checkWrapper.append(checkbox, checkImage);
-
-    const p = document.createElement("p");
-    p.textContent = newItem.value;
-
-    const button = document.createElement("button");
-    button.addEventListener("click", (event) => {
-        button.parentElement.remove();
-        warning.style.display = "flex";
-    });
-
-    li.append(checkWrapper, p, button);
-
-    list.append(li);
-
-    newItem.value = "";
-
+        event.preventDefault();
+        LIST_LOCAL.refreshItems();
+        createItemList(newItem.value);
+        LIST_LOCAL.addItem(newItem.value);
+        LIST_LOCAL.showItems();
+        newItem.value = ""; 
     }
 });
 
@@ -50,6 +77,30 @@ warningBtnClose.addEventListener("click", function() {
 alertBoxBtnClose.addEventListener("click", function() {
     alertBox.style.display = "none";
 })
+
+function createItemList (itemName) {
+        const li = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        const checkImage = document.createElement("span");
+        checkImage.classList.add("check-image");
+        const checkWrapper = document.createElement("span");
+        checkWrapper.classList.add("check-wrapper");
+        checkWrapper.append(checkbox, checkImage);
+        const p = document.createElement("p");
+        p.textContent = itemName;
+        const button = document.createElement("button");
+        button.addEventListener("click", removeItemList);
+        li.append(checkWrapper, p, button);
+        list.append(li);
+}
+
+function removeItemList (event) {
+    event.target.parentNode.remove();
+    warning.style.display = "flex";
+    LIST_LOCAL.removeItem(event.target.previousElementSibling.textContent);
+    LIST_LOCAL.showItems();
+};
 
 
 
