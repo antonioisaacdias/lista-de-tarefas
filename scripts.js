7aa2fdec2410d9af1e6a597c250b8bdd6310692a
antonioisaacdias <antonioisaacvd@gmail.com>
2024-11-03 16:54:32 -0300

    Warning e validações de nova tarefa vazia.

--- scripts.js
+++ scripts.js
@@ -3,10 +3,16 @@ const list = document.querySelector("ul");
 const form = document.querySelector("form");
 const warning = document.querySelector(".warning-wrapper");
 const warningBtnClose = document.querySelector(".warning-wrapper button");
+const alertBox = document.querySelector(".alert");
+const alertBoxBtnClose = document.querySelector(".alert-box button")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    
+
+    if (newItem.value == "") {
+        alertBox.style.display = "flex";
+    } else {
+            
     const li = document.createElement("li");
 
     const checkbox = document.createElement("input");
@@ -32,12 +38,18 @@ form.addEventListener("submit", (event) => {
 
     list.append(li);
 
+    newItem.value = "";
+
+    }
 });
 
 warningBtnClose.addEventListener("click", function() {
     warning.style.display = "none";
 });
 
+alertBoxBtnClose.addEventListener("click", function() {
+    alertBox.style.display = "none";
+})
 
 
 
